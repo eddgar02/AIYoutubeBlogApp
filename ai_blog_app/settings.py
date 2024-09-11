bb45b79d66c9879c0a5812ba1c27ac7848bb09b0
edgarlarasanchez <edgar.lara7681@gmail.com>
2024-09-10 22:29:12 -0500

    set up local database

--- ai_blog_app/settings.py
+++ ai_blog_app/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "ai_blog_app.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": 'my_blog_posts',
+        "USER": 'eddgar02',
+        "PASSWORD": 'Purple2002!',
+        "HOST" : 'localhost',
+        "PORT": '5432',
     }
 }
 
