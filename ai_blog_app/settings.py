7f07689ba0880698b654fc78a0490147c8bfb9a0
edgarlarasanchez <edgar.lara7681@gmail.com>
2024-09-12 18:05:55 -0500

    Added return button

--- ai_blog_app/settings.py
+++ ai_blog_app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,14 +77,9 @@ WSGI_APPLICATION = "ai_blog_app.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": 'my_blog_posts',
-        "USER": 'eddgar02',
-        "PASSWORD": 'Purple2002!',
-        "HOST" : 'localhost',
-        "PORT": '5432',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://my_blog_posts_owner:9nlBjF6pLsSt@ep-jolly-shadow-a5fgndwd.us-east-2.aws.neon.tech/my_blog_posts?sslmode=require'
+    )
 }
 
 
