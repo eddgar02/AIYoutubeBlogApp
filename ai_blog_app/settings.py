0da9c970daa28f83481bdea71574b6876b6e2299
edgarlarasanchez <edgar.lara7681@gmail.com>
2024-09-12 21:17:47 -0500

    update

--- ai_blog_app/settings.py
+++ ai_blog_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-secret-key')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
