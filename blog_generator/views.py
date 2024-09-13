a3c5df69aa2bf416416cf8c4d631498528bf4518
edgarlarasanchez <edgar.lara7681@gmail.com>
2024-09-13 12:11:39 -0500

    exempt and better tailwind styling

--- blog_generator/views.py
+++ blog_generator/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from django.conf import settings
 import json
@@ -26,10 +26,11 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 # Create your views here.
 @login_required
+@csrf_exempt
 def index(request):
     return render(request, 'index.html')
 
-@csrf_protect
+@csrf_exempt
 def generate_blog(request):
     if request.method == 'POST':
         try:
