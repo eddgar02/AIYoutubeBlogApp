56fb58c40a1441d7b3d2704a03014670db6c9444
edgarlarasanchez <edgar.lara7681@gmail.com>
2024-09-13 12:52:46 -0500

    allowed hosts

--- ai_blog_app/settings.py
+++ ai_blog_app/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['91k9frikth.execute-api.us-west-2.amazonaws.com', 'www.91k9frikth.execute-api.us-west-2.amazonaws.com']
+ALLOWED_HOSTS = []
 
 CSRF_TRUSTED_ORIGINS = [
     'https://91k9frikth.execute-api.us-west-2.amazonaws.com'
